4035950643db9b9458803074e7d4767291bb9dbb
Sebastian Springer <sebastian.springer@mayflower.de>
2012-10-03 08:34:27 +0200

    done a lot of more stuff

--- test/cloc2sloc_test.js
+++ test/cloc2sloc_test.js
@@ -1,4 +1,5 @@
-var cloc2sloc = require('../lib/cloc2sloc.js');
+var Cloc2sloc = require('../lib/cloc2sloc.js'),
+    fs = require('fs');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -20,7 +21,7 @@ var cloc2sloc = require('../lib/cloc2sloc.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+/*exports['awesome'] = {
   setUp: function(done) {
     // setup here
     done();
@@ -31,4 +32,57 @@ exports['awesome'] = {
     test.equal(cloc2sloc.awesome(), 'awesome', 'should be awesome.');
     test.done();
   }
+};*/
+
+console.log(Cloc2sloc);
+
+exports['setup'] = {
+    setUp: function(done) {
+        done();
+    },
+    'set input File': function(test) {
+        var cloc2sloc = new Cloc2sloc();
+        cloc2sloc.setInputFile(__dirname + '/../data/input.xml');
+        test.equal(cloc2sloc.inputFile, __dirname + '/../data/input.xml');
+        test.done();
+    },
+    'set output File': function(test) {
+        var cloc2sloc = new Cloc2sloc();
+        cloc2sloc.setOutputFile(__dirname + '/../data/output.sc');
+        test.equal(cloc2sloc.outputFile, __dirname + '/../data/output.sc');
+        test.done();
+    },
+    'fire excpetion, if input file does not exist': function(test) {
+        var cloc2sloc = new Cloc2sloc();
+        test.throws(function() {cloc2sloc.setInputFile(__dirname + '/../data/nonexistant.xml')});
+        test.done();
+    },
+    'check output header': function(test) {
+        var outputFile = __dirname + '/../data/output.sc',
+            cloc2sloc = new Cloc2sloc();
+        if (fs.existsSync(outputFile)) {
+            fs.unlinkSync(outputFile);
+        }
+        cloc2sloc.setOutputFile(outputFile);
+        cloc2sloc.writeHeader(cloc2sloc.getHeader());
+        var expected = 'Creating filelist for source\nCategorizing files.\nComputing results.\n\n\n';
+        var actual = fs.readFileSync(outputFile);
+        test.equal(actual, expected);
+        test.done();
+    },
+    'check format output line': function(test) {
+        var input = {
+            "code": "14",
+            "language": "Javascript",
+            "name": "/tmp/testfile.js"
+            },
+            expected = "14\tJS\tsource\t/tmp/testfile.js\n"
+            cloc2sloc = new Cloc2sloc(),
+            actual = cloc2sloc.constructLine(input);
+
+        test.equal(actual, expected);
+        test.done();
+
+
+    }
 };
